c7498c95a4e65d4e6689f0c90638bcd1e11b5cb3
Brad Patterson <Bradstar879@yahoo.com>
2016-11-08 19:53:08 -0600

    Main Screen
    1st main screen

--- app/src/main/java/com/cs465/uiuc/cs465_project/MainActivity.java
+++ app/src/main/java/com/cs465/uiuc/cs465_project/MainActivity.java
@@ -1,7 +1,17 @@
 package com.cs465.uiuc.cs465_project;
 
+import android.graphics.Color;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ListView;
+
+import com.github.mikephil.charting.charts.BarChart;
+import com.github.mikephil.charting.data.BarData;
+import com.github.mikephil.charting.data.BarDataSet;
+import com.github.mikephil.charting.data.BarEntry;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +19,22 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        BarChart barChart = (BarChart)findViewById(R.id.chart);
+        List<BarEntry> entries = new ArrayList<BarEntry>();
+        ListView listView = (ListView)findViewById(R.id.list_view);
+
+
+        for(int i = 0; i < 7; i++) {
+            entries.add(new BarEntry(i, i));
+        }
+
+        BarDataSet dataSet = new BarDataSet(entries, "Usage");
+        dataSet.setColor(Color.BLUE);
+        dataSet.setValueTextColor(Color.BLACK);
+
+        BarData data = new BarData(dataSet);
+        barChart.setData(data);
+        //barChart.invalidate();
     }
 }
