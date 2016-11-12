db2a882325d89ce91a295a96418573b9196a4f86
mjmalon2 <michaelmaloney@wirelessprv-10-195-182-26.near.illinois.edu>
2016-11-11 19:15:46 -0600

    added goals screen

---
+++ app/src/main/java/com/cs465/uiuc/cs465_project/GoalsActivity.java
@@ -0,0 +1,14 @@
+package com.cs465.uiuc.cs465_project;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.Menu;
+
+
+public class GoalsActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_goals);
+    }
+}
