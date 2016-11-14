c5a52557bdceac52a519b6e5c9c47788aafd699f
Brad Patterson <Bradstar879@yahoo.com>
2016-11-14 00:57:53 -0600

    Swipable Graphs
    Bar graph can be swiped and x-axis is labeled

--- app/src/main/java/com/cs465/uiuc/cs465_project/MainActivity.java
+++ app/src/main/java/com/cs465/uiuc/cs465_project/MainActivity.java
@@ -9,40 +9,64 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.MotionEvent;
 import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
 import android.widget.ListView;
 
 import com.github.mikephil.charting.charts.BarChart;
+import com.github.mikephil.charting.components.AxisBase;
+import com.github.mikephil.charting.components.Description;
+import com.github.mikephil.charting.components.XAxis;
 import com.github.mikephil.charting.data.BarData;
 import com.github.mikephil.charting.data.BarDataSet;
 import com.github.mikephil.charting.data.BarEntry;
+import com.github.mikephil.charting.formatter.IAxisValueFormatter;
+import com.github.mikephil.charting.listener.ChartTouchListener;
+import com.github.mikephil.charting.listener.OnChartGestureListener;
 import com.github.mikephil.charting.utils.ColorTemplate;
 
 import java.util.ArrayList;
 import java.util.List;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity{
     FragmentManager mFragmentManager;
     FragmentTransaction mFragmentTransection;
     PreferenceFragment mPrefsFragment;
+    BarChart barChart;
+    List<BarEntry> entries = new ArrayList<BarEntry>();
+    List<BarEntry> entries2 = new ArrayList<BarEntry>();
+    List<BarEntry> entries3 = new ArrayList<BarEntry>();
+    List<BarEntry> entries4 = new ArrayList<BarEntry>();
+    List<BarEntry> entries5 = new ArrayList<BarEntry>();
+    List<BarEntry> entries6 = new ArrayList<BarEntry>();
+    List<BarEntry> entries7 = new ArrayList<BarEntry>();
+    List<BarEntry> entries8 = new ArrayList<BarEntry>();
+    List<BarEntry> entries9 = new ArrayList<BarEntry>();
+    List<BarEntry> entries10 = new ArrayList<BarEntry>();
+    BarDataSet dataSet;
+    BarData data;
+    int timeView = 0;
+    XAxis xAxis;
+    String[][] labels;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
-        BarChart barChart = (BarChart)findViewById(R.id.chart);
-        List<BarEntry> entries = new ArrayList<BarEntry>();
-        List<BarEntry> entries2 = new ArrayList<BarEntry>();
-        List<BarEntry> entries3 = new ArrayList<BarEntry>();
-        List<BarEntry> entries4 = new ArrayList<BarEntry>();
-        List<BarEntry> entries5 = new ArrayList<BarEntry>();
-        List<BarEntry> entries6 = new ArrayList<BarEntry>();
-        List<BarEntry> entries7 = new ArrayList<BarEntry>();
-        List<BarEntry> entries8 = new ArrayList<BarEntry>();
-        List<BarEntry> entries9 = new ArrayList<BarEntry>();
-        List<BarEntry> entries10 = new ArrayList<BarEntry>();
-
-//        ListView listView = (ListView)findViewById(R.id.list);
+        barChart = (BarChart)findViewById(R.id.chart);
+        barChart.setHighlightPerTapEnabled(false);
+        barChart.setHighlightPerDragEnabled(false);
+        Description d = new Description();
+        d.setText("");
+        barChart.setDescription(d);
+        labels = new String[3][];
+        labels[0] = new String[]{"4AM", "8AM", "12PM", "4PM", "8PM", "12AM"};
+        labels[1] = new String[]{"Su", "M", "Tu", "W", "Th", "F"};
+        labels[2] = new String[]{"11/1", "11/6", "11/11", "11/16", "11/21", "11/26"};
+        xAxis = barChart.getXAxis();
+        xAxis.setValueFormatter(new MyXAxisValueFormatter(labels[0]));
 
         for(int i = 0; i < 6; i++) {
             entries.add(new BarEntry(i, (float)(Math.random()*2 + 0.5)));
@@ -56,29 +80,76 @@ public class MainActivity extends AppCompatActivity {
             entries9.add(new BarEntry(i, (float)(Math.random()*2 + 0.5)));
             entries10.add(new BarEntry(i, (float)(Math.random()*2 + 0.5)));
         }
-        BarDataSet dataSet = new BarDataSet(entries, "Daily");
+        dataSet = new BarDataSet(entries, "Hourly");
         dataSet.setColors(ColorTemplate.COLORFUL_COLORS);
         dataSet.setValueTextColor(Color.BLACK);
 
-        ArrayList<String> labels = new ArrayList<String>();
-        labels.add(0, "4AM");
-        labels.add(0, "8AM");
-        labels.add(0, "12PM");
-        labels.add(0, "4PM");
-        labels.add(0, "8PM");
-        labels.add(0, "12AM");
-
-        BarData data = new BarData(dataSet);
+        data = new BarData(dataSet);
+        barChart.setScaleEnabled(false);
         barChart.setData(data);
         ListView faves = (ListView)findViewById(R.id.list_view);
         faves.setItemsCanFocus(true);
+        barChart.animateY(2000);
 
-        //barChart.animateY(5000);
-        //barChart.invalidate();
+        OnChartGestureListener gestureListener = new OnChartGestureListener() {
+
+            public void onChartGestureStart(MotionEvent me, ChartTouchListener.ChartGesture lastPerformedGesture) {}
+            public void onChartGestureEnd(MotionEvent me, ChartTouchListener.ChartGesture lastPerformedGesture) {}
+            public void onChartLongPressed(MotionEvent me) {}
+            public void onChartDoubleTapped(MotionEvent me) {}
+            public void onChartSingleTapped(MotionEvent me) {}
+            public void onChartFling(MotionEvent me1, MotionEvent me2, float velocityX, float velocityY) {
+                if((velocityX<-.5f && timeView == 0) || (velocityX>.5f && timeView == 2)) {
+                    dataSet = new BarDataSet(entries2, "Daily");
+                    dataSet.setColors(ColorTemplate.COLORFUL_COLORS);
+                    data = new BarData(dataSet);
+                    barChart.setData(data);
+                    xAxis.setValueFormatter(new MyXAxisValueFormatter(labels[1]));
+                    timeView = 1;
+                }
+                else if(velocityX>.5f && timeView == 1) {
+                    dataSet = new BarDataSet(entries, "Hourly");
+                    dataSet.setColors(ColorTemplate.COLORFUL_COLORS);
+                    data = new BarData(dataSet);
+                    barChart.setData(data);
+                    xAxis.setValueFormatter(new MyXAxisValueFormatter(labels[0]));
+                    timeView = 0;
+                }
+                else if(velocityX<-.5f && timeView == 1) {
+                    dataSet = new BarDataSet(entries3, "Monthly");
+                    dataSet.setColors(ColorTemplate.COLORFUL_COLORS);
+                    data = new BarData(dataSet);
+                    barChart.setData(data);
+                    xAxis.setValueFormatter(new MyXAxisValueFormatter(labels[2]));
+                    timeView = 2;
+                }
+            }
+
+            public void onChartScale(MotionEvent me, float scaleX, float scaleY) {}
+            public void onChartTranslate(MotionEvent me, float dX, float dY) {}};
+
+        barChart.setOnChartGestureListener(gestureListener);
+       /* ArrayAdapter<String> adapter = new ArrayAdapter<String>(this, R.layout.activity_main, R.id.list_view);
+        faves.setAdapter(adapter);
+        faves.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
+                    if(i == 0) dataSet = new BarDataSet(entries, "Daily");
+                    else if(i == 1) dataSet = new BarDataSet(entries2, "Daily");
+                    data = new BarData(dataSet);
+                    barChart.setData(data);
+                    barChart.invalidate();
+            }
+        });*/
     }
 
-    public void onListItemClick(ListView l, View v, int position, long id) {
-
+    public class MyXAxisValueFormatter implements IAxisValueFormatter{
+        private String[] mValues;
+    public MyXAxisValueFormatter(String[] values) {this.mValues=values;}
+    public String getFormattedValue(float value, AxisBase axis) {
+        return  mValues[(int)value];
+    }
+    public int getDecimalDigits() {return 0;}
     }
 
     @Override
