8db74e5d45bf51f9883469f6aaf3af236c63dbd4
Averrie <xhuang53@illinois.edu>
2016-11-17 00:06:48 -0600

    fix app screen visibility bug

--- app/src/main/java/com/cs465/uiuc/cs465_project/AppScreen.java
+++ app/src/main/java/com/cs465/uiuc/cs465_project/AppScreen.java
@@ -10,8 +10,6 @@ public class AppScreen extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_app_screen);
-
-
     }
 
     public void favToggle(View v) {
